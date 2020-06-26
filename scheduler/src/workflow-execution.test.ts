a039a22e8f26097ef3ee790ab24e418ca9a46ad3
Georg Schwarz <dev@georg-schwarz.com>
2020-06-26 16:07:25 +0200

    Scheduler: remove functionality for transformation, storage, and notification

--- scheduler/src/workflow-execution.test.ts
+++ scheduler/src/workflow-execution.test.ts
@@ -1,27 +1,12 @@
 /* eslint-env jest */
 import * as PipelineExecution from './workflow-execution'
 import * as AdapterClient from './clients/adapter-client'
-import * as StorageClient from './clients/storage-client'
-import * as TransformationClient from './clients/transformation-client'
-import * as NotificationClient from './clients/notification-client'
-import * as CoreClient from './clients/core-client'
-import PipelineConfig from './interfaces/pipeline-config'
-import NotificationConfig from './interfaces/notification-config'
 import AdapterResponse from '@/interfaces/adapter-response'
 import DatasourceConfig from './interfaces/datasource-config'
-import TransformationConfig from './interfaces/transformation-config'
 
 jest.mock('./clients/adapter-client')
-jest.mock('./clients/storage-client')
-jest.mock('./clients/transformation-client')
-jest.mock('./clients/notification-client')
-jest.mock('./clients/core-client')
 
-const mockGetPipelinesForDatasource = CoreClient.getCachedPipelinesByDatasourceId as jest.Mock
 const mockExecuteAdapter = AdapterClient.executeAdapter as jest.Mock
-const mockExecuteTransformation = TransformationClient.executeTransformation as jest.Mock
-const mockExecuteStorage = StorageClient.executeStorage as jest.Mock
-const mockExecuteNotification = NotificationClient.executeNotification as jest.Mock
 
 const adapterResponse: AdapterResponse = {
   id: 42,
@@ -40,87 +25,22 @@ afterEach(() => {
   jest.clearAllMocks()
 })
 
-test('Should execute pipeline once', async () => {
+test('Should execute adapter once', async () => {
   const datasourceConfig = generateDatasourceConfig(false)
-  const transformation = { dataLocation: '/data/42' }
-  const pipelineConfig = generatePipelineConfig(transformation, [])
-
-  mockGetPipelinesForDatasource.mockReturnValue([pipelineConfig]) // register pipeline to follow datasource import
-
   mockExecuteAdapter.mockResolvedValue(adapterResponse)
-  mockExecuteTransformation.mockResolvedValue({ data: transformedData })
 
   await PipelineExecution.execute(datasourceConfig)
 
   expect(mockExecuteAdapter).toHaveBeenCalledWith(datasourceConfig)
-
-  expect(mockGetPipelinesForDatasource).toHaveBeenCalledWith(datasourceConfig.id)
-
-  expect(mockExecuteTransformation).toHaveBeenCalledWith(transformation)
-  expect(mockExecuteTransformation).toHaveBeenCalledTimes(1)
-
-  expect(mockExecuteStorage).toHaveBeenCalledWith(pipelineConfig, transformedData)
 })
 
-test('Should execute pipeline periodic', async () => {
+test('Should execute adapter periodicly', async () => {
   const datasourceConfig = generateDatasourceConfig(true)
-  const transformation = { dataLocation: 'data/42' }
-  const pipelineConfig = generatePipelineConfig(transformation, [])
-
-  mockGetPipelinesForDatasource.mockReturnValue([pipelineConfig]) // register pipeline to follow datasource import
-
   mockExecuteAdapter.mockResolvedValue(adapterResponse)
-  mockExecuteTransformation.mockResolvedValue({ data: transformedData })
 
   await PipelineExecution.execute(datasourceConfig)
 
   expect(mockExecuteAdapter).toHaveBeenCalledWith(datasourceConfig)
-
-  expect(mockGetPipelinesForDatasource).toHaveBeenCalledWith(datasourceConfig.id)
-
-  expect(mockExecuteTransformation).toHaveBeenCalledWith(transformation)
-  expect(mockExecuteStorage).toHaveBeenCalledWith(pipelineConfig, transformedData)
-})
-
-test('Should not execute undefined transformation', async () => {
-  const datasourceConfig = generateDatasourceConfig(false)
-  const pipelineConfig = generatePipelineConfig(undefined, []) // transformation undefined
-
-  mockExecuteAdapter.mockResolvedValue(adapterResponse)
-
-  await PipelineExecution.execute(datasourceConfig)
-
-  expect(mockExecuteNotification).not.toBeCalled()
-})
-
-test('Should trigger notifications', async () => {
-  const notification1: NotificationConfig = {
-    type: 'WEBHOOK',
-    data: { value1: 1 },
-    dataLocation: 'some.where/over/the/rainbow'
-  }
-  const notification2: NotificationConfig = {
-    type: 'SLACK',
-    data: { schtring: 'text' },
-    dataLocation: 'way.up/high'
-  }
-  const notification3: NotificationConfig = {
-    type: 'FCM',
-    data: { schtring: 'text' },
-    dataLocation: 'way.up/high'
-  }
-  const transformation: TransformationConfig = {
-    dataLocation: 'hier'
-  }
-  const datasourceConfig = generateDatasourceConfig(false)
-  const pipelineConfig = generatePipelineConfig(transformation, [notification1, notification2, notification3])
-
-  mockGetPipelinesForDatasource.mockReturnValue([pipelineConfig]) // register pipeline to follow datasource import
-  mockExecuteAdapter.mockResolvedValue(adapterResponse)
-
-  await PipelineExecution.execute(datasourceConfig)
-
-  expect(mockExecuteNotification).toHaveBeenCalledTimes(3)
 })
 
 function generateDatasourceConfig (periodic = true): DatasourceConfig {
@@ -146,20 +66,5 @@ function generateDatasourceConfig (periodic = true): DatasourceConfig {
       creationTimestamp: new Date(Date.now() + 5000),
       license: 'license 123'
     },
-  }
-}
-
-function generatePipelineConfig (
-  transformation: TransformationConfig | undefined, notifications: NotificationConfig[]): PipelineConfig {
-  return {
-    id: 555,
-    datasourceId: 123,
-    transformation: transformation,
-    metadata: {
-      displayName: 'pipeline 555',
-      creationTimestamp: new Date(Date.now() + 5000),
-      license: 'license 555'
-    },
-    notifications: notifications
   }
 }
--- scheduler/src/workflow-execution.ts
+++ scheduler/src/workflow-execution.ts
@@ -1,12 +1,6 @@
 import * as AdapterClient from './clients/adapter-client'
-import * as CoreClient from './clients/core-client'
-import * as TransformationClient from './clients/transformation-client'
-import * as NotificationClient from './clients/notification-client'
-import * as StorageClient from './clients/storage-client'
-
 import DatasourceConfig from './interfaces/datasource-config'
-import PipelineConfig from './interfaces/pipeline-config'
-import NotificationConfig from './interfaces/notification-config'
+
 import { AxiosError } from 'axios'
 import AdapterResponse from '@/interfaces/adapter-response'
 
@@ -14,20 +8,6 @@ export async function execute (datasourceConfig: DatasourceConfig, maxRetries = 
   // adapter
   const adapterResponse: AdapterResponse =
       await retryableExecution(executeAdapter, datasourceConfig, `Executing adapter for datasource ${datasourceConfig.id}`)
-
-  // pipeline
-  const followingPipelines = await CoreClient.getCachedPipelinesByDatasourceId(datasourceConfig.id)
-  for (let pipelineConfig of followingPipelines) {
-    const transformedData =
-        await retryableExecution(executeTransformation, { pipelineConfig: pipelineConfig, dataLocation: adapterResponse.location }, `Executing transformatins for pipeline ${pipelineConfig.id}`)
-
-    const dataLocation =
-        await retryableExecution(executeStorage, { pipelineConfig: pipelineConfig, data: transformedData }, `Storing data for pipeline ${pipelineConfig.id}`)
-
-    pipelineConfig.notifications.map(async notificationConfig => {
-      await retryableExecution(executeNotification, { notificationConfig: notificationConfig, pipelineConfig: pipelineConfig, data: transformedData, dataLocation: dataLocation }, `Notifying clients for pipeline ${pipelineConfig.id}`)
-    })
-  }
 }
 
 async function retryableExecution<T1, T2>(func: (arg: T1) => Promise<T2>, args: T1, description: string, maxRetries = 3) : Promise<T2> {
@@ -54,49 +34,6 @@ async function executeAdapter (dataousrceConfig: DatasourceConfig): Promise<Adap
   const importedData = await AdapterClient.executeAdapter(dataousrceConfig)
   console.log(`Sucessful import via Adapter for Datasource ${dataousrceConfig.id}`)
   return importedData
-}
-
-async function executeTransformation (args: { pipelineConfig: PipelineConfig, dataLocation: string }): Promise<object> {
-  const pipelineConfig = args.pipelineConfig
-  const dataLocation = args.dataLocation
-
-  console.log(`Execute Pipeline Transformation ${pipelineConfig.id}`)
-  if (!pipelineConfig.transformation) {
-    pipelineConfig.transformation = {
-      dataLocation: AdapterClient.ADAPTER_SERVICE_URL + dataLocation
-    }
-    console.log('Transformation undefined. ')
-  }
-  try {
-    pipelineConfig.transformation.dataLocation = AdapterClient.ADAPTER_SERVICE_URL + dataLocation
-    const jobResult = await TransformationClient.executeTransformation(pipelineConfig.transformation)
-    return jobResult.data
-  } catch (e) {
-    handleError(e)
-    throw new Error('Failed to transform Data via Transformation Service')
-  }
-}
-
-async function executeStorage (args: { pipelineConfig: PipelineConfig, data: object }): Promise<string> {
-  const pipelineConfig = args.pipelineConfig
-  const data = args.data
-
-  console.log(`Storing data for ${pipelineConfig.id}`)
-  const dataLocation = await StorageClient.executeStorage(pipelineConfig, data)
-  console.log(`Sucessfully stored Data for Pipeline ${pipelineConfig.id}`)
-  return dataLocation
-}
-
-async function executeNotification (args: { notificationConfig: NotificationConfig, pipelineConfig: PipelineConfig, data: object, dataLocation: string }): Promise<void> {
-  const notificationConfig = args.notificationConfig
-  const pipelineConfig = args.pipelineConfig
-
-  notificationConfig.data = args.data
-  notificationConfig.dataLocation = args.dataLocation
-  notificationConfig.pipelineId = pipelineConfig.id
-  notificationConfig.pipelineName = pipelineConfig.metadata.displayName
-  await NotificationClient.executeNotification(notificationConfig)
-  console.log(`Successfully delivered notification request to transformation-service for ${pipelineConfig.id}`)
 }
 
 function sleep (ms: number): Promise<void> {
